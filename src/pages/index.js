fa5ee34b73a7bd31ae9dae0ef9ef86381a3237c3
Justin <justinkmpeter@gmail.com>
2024-01-10 22:34:42 -0600

    setup auth and spotify-web-api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
+import React, {useEffect} from "react";
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {useSession} from "next-auth/react";
+import useSpotify from "@/utils/useSpotify";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const spotifyApi = useSpotify()
+  const {data: session} = useSession()
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      console.log("session", session)
+    }
+  }, [session, spotifyApi])
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
